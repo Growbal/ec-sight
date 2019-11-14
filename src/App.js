064d9ccd1db2068e80c710c3ff2ff20038d25aa3
hisatsgs <momiji.winter21@gmail.com>
2019-11-14 15:16:52 +0900

    画面のレイアウトをgridに変更

--- src/App.js
+++ src/App.js
@@ -2,25 +2,11 @@ import React from 'react';
 import './App.css';
 
 import  Header from './Header';
-import Top from './components/Top'
+import Top from './components/Top';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Header />
       <Top />
     </div>
