6b2f69b84bd579efae67c5175b9a3027306b13a6
hisatsgs <momiji.winter21@gmail.com>
2019-11-24 17:50:22 +0900

    モーダルウィンドウの作成

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,21 @@
 import React , { Component } from 'react';
+import Modal from 'react-modal';
 
 import './Login.css';
+
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)',
+    }
+};
+
+Modal.setAppElement('#root');
 
 class Login extends Component{
     constructor(props){
@@ -8,22 +23,51 @@ class Login extends Component{
 
         this.state = {
             email: "",
-            password: ""
-        }
+            password: "",
+            modalIsOpen: false
+        };
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     render(){
         return(
             <React.Fragment>
-                <div className="login">
-                    <form className='login-list' onSubmit={ this.handleSubmit }>
-                        <input type="text" placeholder='メールアドレスを入力' value = {this.state.email } onChange = { this.handleChangeEmail } />
-                        <input type="text" placeholder='ぱすわーど' value = { this.state.password } onChange = { this.handleChangePassword }/>
-                        <button>ろぐいん</button>
-                    </form>
+                <div className="menu">
+                    <div className="registration">
+                        <p>登録する</p>
+                    </div>
+                    <div className="login">
+                        <p onClick={ this.openModal }>ろぐいん</p>
+                        <Modal                          
+                            isOpen={this.state.modalIsOpen}
+                            onAfterOpen={this.afterOpenModal}
+                            onRequestClose={this.closeModal}
+                            style={customStyles}>
+                            <form className='login-list' onSubmit={ this.handleSubmit }>
+                                <input type="text" placeholder='めーる' value = {this.state.email } onChange = { this.handleChangeEmail } />
+                                <input type="text" placeholder='ぱすわーど' value = { this.state.password } onChange = { this.handleChangePassword }/>
+                                <button>ろぐいん</button>
+                            </form>
+                        </Modal>
+                    </div>
                 </div>
             </React.Fragment>
         )
+    }
+
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    afterOpenModal() {
+        // this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
     }
 
     handleChangeEmail = event => {
